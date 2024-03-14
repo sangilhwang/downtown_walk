5fe23d211b376382a36e7e758d7e5f2a9c7217ec
Kim Minsu <freekms95@gmail.com>
2024-03-14 16:53:52 +0900

    웹 그래프 출력
    1. 웹 그래프 출력 확인
    2. 임대료데이터 전처리 및  csv정리

--- web/analysis_data/views.py
+++ web/analysis_data/views.py
@@ -1,5 +1,44 @@
 from django.shortcuts import render
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from analysis_data.models import EmpData
+
 
 # Create your views here.
 def main(request):
-    return render(request, "main.html")
+    queryset = EmpData.objects.all().order_by("smallfirstfloor").values("borough", "smallfirstfloor", "emptopopul")
+    labels = [item["borough"] for item in queryset]
+    values1 = [item["smallfirstfloor"] for item in queryset]
+    values2 = [item["emptopopul"] for item in queryset]
+    context = {
+        "labels" : labels,
+        "values1" : values1,
+        "values2" : values2,
+        }
+    return render(request, "main.html", context)
+
+
+
+
+
+
+
+    # sns.set(style="whitegrid")
+    # fig = plt.figure(figsize = (15, 8))
+
+    # ax1 = fig.add_subplot(1, 1, 1)
+    # ax2 = ax1.twinx()
+
+    # groups = len(df.index)
+    # idx = np.arange(groups)
+
+    # sns.barplot(x = df["borough"], y = df["smallfirstfloor"], ax = ax1)
+    # ax1.set_ylim(20, 90)
+    # ax2.plot(df["borough"], df["emptopopul"], "ko-")
+
+    # ax1.set_xticklabels([i for i in df["borough"]], rotation = 45, size = 12)
+
+    # img_path = "static/sample_plot.png"
+    # plt.savefig(img_path)
--- web/downtown_walk/settings.py
+++ web/downtown_walk/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = BASE_DIR / "templates"
 
 # Quick-start development settings - unsuitable for production
@@ -117,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
