50dc5d5e10b35fc44a69fe46b421e18a96c9dc68
Lee Wooyoung <sprtms6159@naver.com>
2024-03-15 17:59:32 +0900

    웹페이지 작업
    1. 메인페이지 작업 완료
    2. 분석 시각화 페이지
      - 데이터 그래프 유형선택
      - 그래프 시각화 위치 고정

--- web/downtown_walk/urls.py
+++ web/downtown_walk/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
 from . import views
 
+app_name = "main"
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.main),
+    path("", views.main, name="main_page"),
     path("plot/", include("analysis_data.urls")),
 ]
